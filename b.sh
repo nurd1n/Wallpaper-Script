f301150b5db7118dd9154252f8e1ad07763bcab8
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 18:53:45 +0700

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,6 @@
+mkdir image/asin
+shuf -n 1 c.sh | sed 's/asir/asin/g' > delete.txt
+echo "sed -e 's/st1/$(shuf -i 20-95 -n 1)/g' -e 's/st2/$(shuf -i 20-95 -n 1)/g' delete.txt" | bash - | bash -
+for f in image/asin/*.jpg; do mv -v $f `echo $f | tr '[A-Z]' '[a-z]'`; done
+for f in image/asin/*.jpg; do mv -v $f `echo $f | tr '[A-Z]' '[a-z]'`; done
+rm -r -f asin
