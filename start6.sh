5294692f2f671b993c058736226adfb9655b9f8d
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 20:30:16 +0700

    Update start6.sh

--- start6.sh
+++ start6.sh
@@ -6,4 +6,4 @@ find ./image/* -type d | cut -c 9- > /home/wallpaper/artikel/data/folder.txt
 #total isi untuk attachment
 find ./image/*/* -type f | cut -c 9- > /home/wallpaper/attachment/data/folder.txt
 #pindah folder gambar ke folder don di attachment
-mv */ /home/wallpaper/attachment/done
+mv image/* /home/wallpaper/attachment/done
