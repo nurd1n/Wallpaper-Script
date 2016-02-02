5b0dacb70ff5ec5b53ea881d34e200cf0ce3ba8a
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 11:36:04 +0700

    Create start3.sh

---
+++ start3.sh
@@ -0,0 +1,6 @@
+IFS=';'
+while read f1 f2; do
+    echo "sed -e 's|urlbing|$f1|g' -e 's|asin|$f2|g' build.sh > build2.sh" | bash -
+    echo "chmod 755 build2.sh" | bash -
+    echo "./build2.sh" | bash -
+done < url
