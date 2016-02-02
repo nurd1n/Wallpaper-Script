1c8731145fd3259fb065ade72033ee385a90d280
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 10:55:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 echo "deletekeyword" | sed -e 's/ /+/g' > deletek.txt
 echo "sed -e 's/keyword/$(< \deletek.txt)/g' url.sh > deleteurl.txt" | bash
 cat deleteurl.txt | bash -
-echo "wc -l < Report/$(< \deletek.txt).txt > deleteresult.txt" | bash -
+echo "wc -l < report/$(< \deletek.txt).txt > deleteresult.txt" | bash -
 cat deleteresult.txt | awk '$0="deletekeyword,"$0' >> result.txt
 sleep 1
 # delete & clear history
