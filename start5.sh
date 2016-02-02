d0eb50a855c03a6eb90c670ebeccc152e0f5bc6f
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 19:00:48 +0700

    Update start5.sh

--- start5.sh
+++ start5.sh
@@ -1,6 +1,7 @@
 IFS=';'
 while read f1; do
     echo "sed -e 's|asin|$f1|g' b.sh > deleteb.sh" | bash -
+    chmod 755 deleteb.sh
     echo ./deleteb.sh | bash -
     echo "shred -v -n 25 -u -z delete*" | bash -
 done < asin.txt
