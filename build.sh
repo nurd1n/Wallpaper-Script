9cfd0f6ddab15829193a447d5f0fa72cbef52741
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 10:49:05 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "deletekeyword" | sed -e 's/ /+/g' > deletek.txt
+echo "sed -e 's/keyword/$(< \deletek.txt)/g' url.sh > deleteurl.txt" | bash
+cat deleteurl.txt | bash -
+echo "wc -l < Report/$(< \deletek.txt).txt > deleteresult.txt" | bash -
+cat deleteresult.txt | awk '$0="deletekeyword,"$0' >> result.txt
+sleep 1
+# delete & clear history
+find . -maxdepth 1 -type f -name "delete*" -delete
+history -c
