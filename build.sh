afc4ddeb915934cc148ded8294bf3fba4b94365e
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 18:14:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-mkdir asin
-if [ $(find asin -type f | wc -l) -lt "20" ]; then
-  for f in asin/*.jpg; do 
+mkdir done/asin
+if [ $(find done/asin -type f | wc -l) -lt "20" ]; then
+  for f in done/asin/*.jpg; do 
   if [ $(exiftool -s3 -ImageWidth "$f") -z "$VAR" ]; then
   rm -f "$f"
   else
@@ -24,19 +24,19 @@ if [ $(find asin -type f | wc -l) -lt "20" ]; then
   for f in *.JPG; do mv -- "$f" `echo $f | sed -e 's/&amp/-and-/g' -e 's/&/-and-/g' -e 's/ä/a/g' -e 's/ö/o/g' -e 's/ü/u/g' -e 's/[+_,;: (){}]/-/g' -e 's/--/-/g' -e 's/\[/-/g'  -e 's/\]/-/g' | tr -d '\041-\047' | tr -d '\052' | tr -d '\056-\057' | tr -d '\074-\100' | tr -d '\134' | tr -d '\136' | tr -d '\140' | tr -d '\174' | tr -d '\176' | sed -e 's/--/-/g' -e 's/JPG/.JPG/g' -e 's/^-*//g' -e 's/-.JPG//g' -e 's/.JPG/-/g' -e 's/-*$/.JPG/g'`; done
   for f in *.png; do mv -- "$f" `echo $f | sed -e 's/&amp/-and-/g' -e 's/&/-and-/g' -e 's/ä/a/g' -e 's/ö/o/g' -e 's/ü/u/g' -e 's/[+_,;: (){}]/-/g' -e 's/--/-/g' -e 's/\[/-/g'  -e 's/\]/-/g' | tr -d '\041-\047' | tr -d '\052' | tr -d '\056-\057' | tr -d '\074-\100' | tr -d '\134' | tr -d '\136' | tr -d '\140' | tr -d '\174' | tr -d '\176' | sed -e 's/--/-/g' -e 's/png/.png/g' -e 's/^-*//g' -e 's/-.png//g'-e 's/.png/-/g' -e 's/-*$/.png/g'`; done
   for f in *.jpeg; do mv -- "$f" `echo $f | sed -e 's/&amp/-and-/g' -e 's/&/-and-/g' -e 's/ä/a/g' -e 's/ö/o/g' -e 's/ü/u/g' -e 's/[+_,;: (){}]/-/g' -e 's/--/-/g' -e 's/\[/-/g'  -e 's/\]/-/g' | tr -d '\041-\047' | tr -d '\052' | tr -d '\056-\057' | tr -d '\074-\100' | tr -d '\134' | tr -d '\136' | tr -d '\140' | tr -d '\174' | tr -d '\176' | sed -e 's/--/-/g' -e 's/jpeg/.jpeg/g' -e 's/^-*//g' -e 's/-.jpeg//g'-e 's/.jpeg/-/g' -e 's/-*$/.jpeg/g'`; done
-  echo "for a in *.jpg; do mv -- \$a asin/$(< \deletetitle2.txt)-\${a%.jpg}-$(< \deletenumber.txt).jpg; done" | bash -
-  echo "for a in *.JPG; do mv -- \$a asin/$(< \deletetitle2.txt)-\${a%.JPG}-$(< \deletenumber.txt).jpg; done" | bash -
-  echo "for a in *.png; do mv -- \$a asin/$(< \deletetitle2.txt)-\${a%.png}-$(< \deletenumber.txt).jpg; done" | bash -
-  echo "for a in *.jpeg; do mv -- \$a asin/$(< \deletetitle2.txt)-\${a%.jpeg}-$(< \deletenumber.txt).jpg; done" | bash -
-  echo "for a in *.jpg; do cp -- \$a asin/$(< \deletetitle2.txt)-\${a%.jpg}-$(< \deletenumber.txt).jpg; done" | bash -
-  echo "for a in *.JPG; do cp -- \$a asin/$(< \deletetitle2.txt)-\${a%.JPG}-$(< \deletenumber.txt).jpg; done" | bash -
-  echo "for a in *.png; do cp -- \$a asin/$(< \deletetitle2.txt)-\${a%.png}-$(< \deletenumber.txt).jpg; done" | bash -
-  echo "for a in *.jpeg; do cp -- \$a asin/$(< \deletetitle2.txt)-\${a%.jpeg}-$(< \deletenumber.txt).jpg; done" | bash -
-  cd asin
+  echo "for a in *.jpg; do mv -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.jpg}-$(< \deletenumber.txt).jpg; done" | bash -
+  echo "for a in *.JPG; do mv -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.JPG}-$(< \deletenumber.txt).jpg; done" | bash -
+  echo "for a in *.png; do mv -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.png}-$(< \deletenumber.txt).jpg; done" | bash -
+  echo "for a in *.jpeg; do mv -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.jpeg}-$(< \deletenumber.txt).jpg; done" | bash -
+  echo "for a in *.jpg; do cp -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.jpg}-$(< \deletenumber.txt).jpg; done" | bash -
+  echo "for a in *.JPG; do cp -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.JPG}-$(< \deletenumber.txt).jpg; done" | bash -
+  echo "for a in *.png; do cp -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.png}-$(< \deletenumber.txt).jpg; done" | bash -
+  echo "for a in *.jpeg; do cp -- \$a done/asin/$(< \deletetitle2.txt)-\${a%.jpeg}-$(< \deletenumber.txt).jpg; done" | bash -
+  cd done/asin
   for f in *.jpg; do echo "mv -- $f \$(echo $f | sed 's/^-*//')" | bash -; done
   cd -
-  find ./asin/* -wholename "*.jpg" -size -15k -delete
-  find ./asin/* -not -empty -type f -printf '%s\n' | sort -rn | uniq -d | xargs -I{} -n1 find ./asin/* -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate | cut -c 37- | sed -n '1!p' | xargs shred -v -n 25 -u -z
+  find ./done/asin/* -wholename "*.jpg" -size -15k -delete
+  find ./done/asin/* -not -empty -type f -printf '%s\n' | sort -rn | uniq -d | xargs -I{} -n1 find ./done/asin/* -type f -size {}c -print0 | xargs -0 md5sum | sort | uniq -w32 --all-repeated=separate | cut -c 37- | sed -n '1!p' | xargs shred -v -n 25 -u -z
   find . -maxdepth 1 -type f ! -name "*.sh" ! -name "url" ! -name "urlsudah" ! -name "*.py" -delete
   history -c
   clear
