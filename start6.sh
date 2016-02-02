40c929fa84ed2db80e8a7c54e4980cff0180716b
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 20:02:33 +0700

    Update start6.sh

--- start6.sh
+++ start6.sh
@@ -1,7 +1,7 @@
 #delete empty folder
-find . -type d -empty -exec rmdir {} \;
+find ./image/ -type d -empty -exec rmdir {} \;
 #folder, untuk judul artikel
-find . -type d | cut -c 3- > /home/wallpaper/artikel/data/folder.txt
+find ./image/* -type d | cut -c 9- > /home/wallpaper/artikel/data/folder.txt
 ./isifolder.sh
 #total isi untuk attachment
 find ./*/* -type f | cut -c 3- > home/wallpaper/attachment/data/folder.txt
