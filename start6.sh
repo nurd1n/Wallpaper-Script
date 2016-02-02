65c93fea8107ea364f2306c3b34dd17a4257eaf7
Nurdianto <nurdiantopntr@gmail.com>
2016-02-02 20:27:51 +0700

    Update start6.sh

--- start6.sh
+++ start6.sh
@@ -4,6 +4,6 @@ find ./image/* -type d -empty -exec rmdir {} \;
 find ./image/* -type d | cut -c 9- > /home/wallpaper/artikel/data/folder.txt
 ./isifolder.sh
 #total isi untuk attachment
-find ./image/*/* -type f | cut -c 9- > home/wallpaper/attachment/data/folder.txt
+find ./image/*/* -type f | cut -c 9- > /home/wallpaper/attachment/data/folder.txt
 #pindah folder gambar ke folder don di attachment
 mv */ /home/wallpaper/attachment/done
