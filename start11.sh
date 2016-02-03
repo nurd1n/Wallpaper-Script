76bde9ec1f99519f64887834f23618c2271a6785
Nurdianto <nurdiantopntr@gmail.com>
2016-02-03 20:32:21 +0700

    Delete start11.sh

--- start11.sh
+++
@@ -1,4 +0,0 @@
-# tambah awalan & akhiran
-sed -i -e '1i<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\' /home/domain/wordpress/sitemap-attachment.xml
-sed -i -e '1i<?xml version="1.0" encoding="UTF-8"?>\' /home/domain/wordpress/sitemap-attachment.xml
-sed -i '$ a </urlset>' /home/domain/wordpress/sitemap-attachment.xml
