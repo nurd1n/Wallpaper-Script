18af4c42c11af92dbba0eca9803f76886c37a399
Nurdianto <nurdiantopntr@gmail.com>
2016-02-03 15:41:14 +0700

    Update start9.sh

--- start9.sh
+++ start9.sh
@@ -1,5 +1,8 @@
 cd /home/domain/wordpress
 wp post list --post_type=post --fields=ID,post_title --format=csv --allow-root > /home/wallpaper/artikel/domain-post.csv
+cd /home/wallpaper/artikel
 cat domain-post.csv | sed -e 's/ID,post_title//g' -e '/^$/d' -e 's/ /+/g' -e 's/,"/ > \/home\/wallpaper\/attachment\/id\//g' -e 's/.*/echo &/' -e 's/"/.txt/g' | tr '[A-Z]' '[a-z]' > deleteid.sh
 chmod 755 deleteid.sh
 ./deleteid.sh
+find . -maxdepth 1 -type f -name "delete*" -delete
+history -c
