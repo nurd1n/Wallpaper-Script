5d3f862c4c9869fca0d3132dff8cec0045e711eb
Nurdianto <nurdiantopntr@gmail.com>
2016-02-03 14:39:17 +0700

    Update start9.sh

--- start9.sh
+++ start9.sh
@@ -1,2 +1,2 @@
 cd /home/domain/wordpress
-wp post list --post_type=post --fields=post_title,ID --format=csv --allow-root > /home/wallpaper/artikel/domain-post.csv
+wp post list --post_type=post --fields=ID,post_title --format=csv --allow-root > /home/wallpaper/artikel/domain-post.csv
