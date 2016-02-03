05c36b129d71e438ae8d2811c19d8a2a7eb4ff74
Nurdianto <nurdiantopntr@gmail.com>
2016-02-03 19:31:37 +0700

    Update start10.sh

--- start10.sh
+++ start10.sh
@@ -3,4 +3,5 @@ while read f1 f2; do
     echo "sed -e 's/title1/$f1/g' -e 's/title2/$f2/g' build.sh > build2.sh" | bash -
     echo "chmod 755 build2.sh" | bash -
     echo "./build2.sh" | bash -
+    echo "rm -f build2.sh" | bash -    
 done < data/folder.txt
