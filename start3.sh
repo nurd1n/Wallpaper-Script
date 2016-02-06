f97f1e6a94ef68872304608612304a2308abb808
Nurdianto <nurdiantopntr@gmail.com>
2016-02-06 10:43:10 +0700

    Update start3.sh

--- start3.sh
+++ start3.sh
@@ -3,4 +3,5 @@ while read f1 f2; do
     echo "sed -e 's|urlbing|$f1|g' -e 's|asin|$f2|g' build.sh > build2.sh" | bash -
     echo "chmod 755 build2.sh" | bash -
     echo "./build2.sh" | bash -
+    echo "rm -f build2.sh" | bash -
 done < url
