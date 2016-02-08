4cbd2256366747279163e57f42f0de737d0fec8d
Nurdianto <nurdiantopntr@gmail.com>
2016-02-08 20:05:35 +0700

    Update start8.sh

--- start8.sh
+++ start8.sh
@@ -1,1 +1,1 @@
-sed -i -e '1ititle| description| categories| tags| author| id\' /home/domain/wordpress/image/wpallimport/files/wordpress.csv
+sed -i -e '1ititle| description| categories| tags| author| id\' /home/www/domain/wp-content/uploads/wpallimport/files/wordpress.csv
