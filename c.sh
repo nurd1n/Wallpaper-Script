3f9ef7df01a12f28f295e73f30eaaaa6d125bac3
Nurdianto <nurdiantopntr@gmail.com>
2016-02-24 16:44:23 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,3 +1,3 @@
-for f in asir/*.jpg; do ffmpeg -i "${f}" -vf "crop=iw/1.02:ih/1.02,scale='if(gt(a,16/9),3840,-1)':'if(gt(a,16/9),-1,2160)',colorbalance=bh=.1,setdar=st1:st2,hflip" image/"${f}"; done
-for f in asir/*.jpg; do ffmpeg -i "${f}" -vf "crop=iw/1.02:ih/1.02,scale='if(gt(a,16/9),5120,-1)':'if(gt(a,16/9),-1,2880)',colorbalance=bh=.1,setdar=st1:st2,hflip" image/"${f}"; done
-for f in asir/*.jpg; do ffmpeg -i "${f}" -vf "crop=iw/1.02:ih/1.02,scale='if(gt(a,16/9),5760,-1)':'if(gt(a,16/9),-1,3240)',colorbalance=bh=.1,setdar=st1:st2,hflip" image/"${f}"; done
+for f in asir/*.jpg; do ffmpeg -i "${f}" -vf "crop=iw/1.02:ih/1.02,scale='if(gt(a,16/9),720,-1)':'if(gt(a,16/9),-1,405)',colorbalance=bh=.1,setdar=st1:st2,hflip" image/"${f}"; done
+for f in asir/*.jpg; do ffmpeg -i "${f}" -vf "crop=iw/1.02:ih/1.02,scale='if(gt(a,16/9),816,-1)':'if(gt(a,16/9),-1,459)',colorbalance=bh=.1,setdar=st1:st2,hflip" image/"${f}"; done
+for f in asir/*.jpg; do ffmpeg -i "${f}" -vf "crop=iw/1.02:ih/1.02,scale='if(gt(a,16/9),912,-1)':'if(gt(a,16/9),-1,513)',colorbalance=bh=.1,setdar=st1:st2,hflip" image/"${f}"; done
