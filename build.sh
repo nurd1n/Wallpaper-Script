d27cf76771d8c44aadb0b6b305698db9aa38b429
Nurdianto <nurdiantopntr@gmail.com>
2016-02-26 07:47:53 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ cat deletepar3a.txt | awk 1 ORS=' ' | sed -e 's/^/<div style="text-align: justif
 echo "wget -qO- --header=\"Accept: text/html\" --user-agent=\"Googlebot-Image/1.0\" \"http://www.bing.com/videos/search?scope=video&pq=$(echo title1 | sed 's/+2016//g')&sc=1-30&sp=-1&sk=&q=$(echo title1 | sed 's/+2016//g')&qft=+filterui:msite-youtube.com\" | grep -o '<a href=['\"'\"'\"][^\"'\"'\"']*['\"'\"'\"]' | grep 'watch?v' | sed -e 's/^<a href=[\"'\"'\"']//' -e 's/[\"'\"'\"']$//' -e 's/https:\\/\\/www.youtube.com\\/watch?v=//g' | shuf -n 1 > deleteyoutube.txt" | bash -
 echo "<br/><style>.embed-container { position: relative; text-align: center; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe type=\"text/html\" width=\"560\" height=\"315\" src=\"https://www.youtube-nocookie.com/embed/$(cat deleteyoutube.txt)?autohide=1&rel=0&controls=0&modestbranding=1&disablekb=1&theme=light&enablejsapi=1\" frameborder=\"0\"></iframe></div>" >> deletepar3.txt
 # shuffle templates
-echo "<div style="text-align: center"><h3>$(cat deletejudul2.txt)</h3></div>" > deletedes.txt
+echo "<div style=\"text-align: center;\"><h3>$(cat deletejudul2.txt)</h3></div>" > deletedes.txt
 echo "cat template/$(shuf -i 1-3 -n 1)" | bash - | bash -
 # title ask
 cat deletejudul2.txt | sed 's/ /\n/g' | sed -n 1,5p | awk 1 ORS='+' | sed 's/.$//' > deletejudulask.txt
