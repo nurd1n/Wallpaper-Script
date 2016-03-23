70310cd61a1e14b342a78dac783cc0cff1ef68ae
Nurdianto <nurdiantopntr@gmail.com>
2016-03-23 10:04:45 +0700

    Update isifolder.sh

--- isifolder.sh
+++ isifolder.sh
@@ -1,5 +1,5 @@
 #folder + isi folder dalam bentuk txt untuk folder file di artikel
 IFS=';'
 while read f1; do
-    echo "find ./image/$f1/ -type f -mtime -14 -printf '%f\n' > /home/wallpaper/artikel/file/$f1.txt" | bash -
-done < /home/wallpaper/artikel/data/folder.txt
+    echo "find ./image/$f1/ -type f -mtime -14 -printf '%f\n' > /home/wallpaperdomainekstension/artikel/file/$f1.txt" | bash -
+done < /home/wallpaperdomainekstension/artikel/data/folder.txt
