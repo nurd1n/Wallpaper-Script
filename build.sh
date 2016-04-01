73c0661dfd2eff2a95067a0f9a1e979b20428ab9
Nurdianto <nurdiantopntr@gmail.com>
2016-04-01 08:47:46 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -88,7 +88,7 @@ cat deletekeyword1.txt | sed '/^$/d' > deletekeyword.txt
 awk 1 ORS=', ' < deletekeyword.txt > deletekeyword1.txt
 sed -i 's/  / /g' deletekeyword1.txt
 sed -e 's/,,/,/g' -e 's/, ,/,/g' -e 's/,,/,/g' -e 's/ , /, /g' deletekeyword1.txt | sed 's/.$//' | sed 's/.$//' > deletetags2.txt
-echo "exiftool -Title=\"$(cat deletejudul2.txt) - $(cat deletejudul1.txt)\" -Author=\"$(cat deleteauthor1.txt)\" -Description=\"The picture/image of $(cat deletejudul2.txt) - $(cat deletejudul1.txt)\" -Keyword=\"$(cat deletejudul2.txt), $(cat deletejudul1.txt), $(cat deletetags2.txt)\" -Copyright=\"Source http://www.domain.ekstension/image/title2\" done/title1/title2" | bash -
+echo "exiftool -Title=\"$(cat deletejudul2.txt) - $(cat deletejudul1.txt)\" -Author=\"$(cat deleteauthor1.txt)\" -Description=\"The picture/image of $(cat deletejudul2.txt) - $(cat deletejudul1.txt)\" -Keyword=\"$(cat deletejudul2.txt), $(cat deletejudul1.txt)\" -Copyright=\"Source http://www.domain.ekstension/wp-content/uploads/title2\" done/title1/title2" | bash -
 # sql file 1
 echo "cat data/sql1 | sed -e 's|author2|$(cat deleteauthor2.txt)|g' -e 's|tanggal|$(cat deletetanggal.txt)|g' -e 's|waktu|$(cat deletewaktu.txt)|g' >> deletesql.txt" | bash -
 # input description
